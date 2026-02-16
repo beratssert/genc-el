6fb4bfd09fdcdaf180f70e719fdcb7df35e9769e
Berat Sert <berat@example.com>
2026-02-16 16:29:45 +0300

    Initialize Spring Boot backend with Docker composed support

---
+++ backend/src/main/java/com/gencel/backend/GencElApplication.java
@@ -0,0 +1,13 @@
+package com.gencel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GencElApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GencElApplication.class, args);
+    }
+
+}
